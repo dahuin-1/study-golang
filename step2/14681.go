c2fb4d9d821048724c7175aca25f6485bfe5691e
dahuin-1 <dawhite01@naver.com>
2022-01-13 22:47:15 +0900

    add 14681.go

---
+++ step2/14681.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int
+	var y int
+
+	fmt.Scanln(&x)
+	fmt.Scanln(&y)
+
+	if (x > 0 && y > 0) {
+		fmt.Println(1)
+	}else if (x < 0 && y > 0) {
+		fmt.Println(2)
+	}else if (x < 0 && y < 0) {
+		fmt.Println(3)
+	}else{
+		fmt.Println(4)
+	}
+}
