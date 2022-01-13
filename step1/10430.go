086cc9374bd11a5e0d99e399d10d450cdd568ca5
dahuin-1 <dawhite01@naver.com>
2022-01-13 22:07:36 +0900

    add 10430.go

--- step1/10430.go
+++ step1/10430.go
@@ -7,6 +7,9 @@ func main() {
 	var b int
 	var c int
 	fmt.Scanf("%d %d %d", &a, &b, &c)
-
+	fmt.Println((a+b)%c)
+	fmt.Println(((a%c) + (b%c))%c)
+	fmt.Println((a*b)%c)
+	fmt.Println(((a%c) * (b%c))%c)
 }
 
