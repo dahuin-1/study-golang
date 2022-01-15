f1ed8947f86d4ef3403e8c8209cfd3b1def0036c
dahuin-1 <dawhite01@naver.com>
2022-01-15 18:36:04 +0900

    add 2739.go

---
+++ step3/2739.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*func main() {
+	var n int
+	fmt.Scanf("%d",&n)
+	for i := 1; i < 10; i++ {
+		fmt.Println(n," * ",i," = ",n * i)
+	}
+}*/
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+	for i:= 1; i <= 9 ; i ++ {
+		fmt.Printf("%d * %d = %d\n", n, i, n*i)
+	}
+}
