426227f62f12cbb6fc07af80316b3c29efa85de9
dahuin-1 <dawhite01@naver.com>
2022-01-18 02:23:07 +0900

    add 2562-b.go

--- step5/test.go
+++ step5/test.go
@@ -10,16 +10,15 @@ func main() {
 	var numbers = make([]int, 9)
 	var max = 0
 	var maxIdx = 0
-    reader := bufio.NewReader(os.Stdin)
-	for i := 0; i < 9; i++ {
-		//for i := range(numbers) {}
+	reader := bufio.NewReader(os.Stdin)
+	for i := 0; i < len(numbers); i++ {
 		fmt.Fscanf(reader, "%d\n", &numbers[i])
 		if max < numbers[i] {
 			max = numbers[i]
 			maxIdx = i
 		}
 	}
-		fmt.Println(max)
-		fmt.Println(maxIdx + 1)
+	fmt.Println(max)
+	fmt.Println(maxIdx + 1)
 
 }
