284a531185f4d540f534ea31062e3c41e4444c3d
dahuin-1 <dawhite01@naver.com>
2022-01-18 20:10:53 +0900

    add 2577.go

--- step5/2577.go
+++ step5/2577.go
@@ -8,22 +8,18 @@ import (
 
 func main() {
 	var a, b, c int
+	var counts = make([]int, 10)
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Fscanf(reader, "%d\n%d\n%d\n", &a, &b, &c)
-    var result = a * b * c
-	var counts = make([]int, 10)
-	
-	for true { 
-		counts[result%10]++ 
-		result /= 10 
-		if result == 0 {
+	var number = a * b * c
+	for true {
+		counts[number % 10]++ 
+		number = number / 10
+		if number == 0 {
 			break
 		}
 	}
-
 	for i := range counts {
 		fmt.Println(counts[i])
 	}
-
-
 }
