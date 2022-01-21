b693fe30dfbd65690a0236266b87a407b8832237
dahuin-1 <dawhite01@naver.com>
2022-01-21 20:18:10 +0900

    add 1546.go

--- step5/1546.go
+++ step5/1546.go
@@ -12,17 +12,21 @@ func main() {
 	fmt.Fscanln(reader, &n)
 	var inputs = make([]int, n)
 	var max = 0
-	var sum = 0
+	var sum float32 = 0
 	for i := 0; i < n; i++ {
 		fmt.Fscan(reader, &inputs[i])
-		if max < inputs[i] {
+		if  max < inputs[i] {
 			max = inputs[i]
 		}
 	}
-	var newScore = make([]int, n)
+	//fmt.Println(max)
+	var newScore = make([]float32, n)
 	for i := 0; i < n; i++ {
-		newScore[i] = inputs[i] / max * 100
+		//fmt.Println(inputs[i])
+		newScore[i] = float32(inputs[i] / max) * 100
+		fmt.Println(float32(inputs[i] / max))
 		sum += newScore[i]
 	}
-	fmt.Println(sum / n)
+	fmt.Println(sum)
+	fmt.Println(sum / float32(n))
 }
